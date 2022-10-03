75b305b220011f778cf86fea00a823f4b12c09fa
Vanese <vanesebuchanan@gmail.com>
2022-10-02 21:41:58 -0400

    redo

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ let upperCase = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P
 console.log(upperCase)
 let lowerCase = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 console.log(lowerCase)
-let numbers = ["0", "1","2","3","4","5","6","7","8","9"];
-console.log(numbers)
+let number = ["0", "1","2","3","4","5","6","7","8","9"];
+console.log(number)
 let specialChar = ["!", "@","#","$","%","^","&","*","(",")"];
 console.log(specialChar)
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 
 
 // Write password to the #password input
@@ -39,6 +40,7 @@ let generatePassword = function() {
     let passwordNumbers = prompt("Click Ok to include number.");
     let passwordspecialChar = prompt("Click Ok to include special charcter.");
 }
+
 
 
 
