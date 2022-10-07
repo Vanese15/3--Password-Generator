bef293d9297e505c044bb98a3bd20d946f0040ad
Vanese <vanesebuchanan@gmail.com>
2022-10-07 14:34:25 -0400

    still not working, but will keep trying

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-// Array for all character option for password
-let upperCase = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-console.log(upperCase)
-let lowerCase = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-console.log(lowerCase)
-let number = ["0", "1","2","3","4","5","6","7","8","9"];
-console.log(number)
-let specialChar = ["!", "@","#","$","%","^","&","*","(",")"];
-console.log(specialChar)
+function randomInt(min, max) {
+    let randomInt = Math.random()
+    return Math.floor(min + (1 - random))
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+}
 
+function generatePassword() { 
+    let passwordLength = window.prompt("How long do you want your password to be?");
+   
+    if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password must be between 8 and 128 characters.");
+    return
+} 
+    // else if (passwordLength>128) {
+    // window.alert("Password maximum is 128 characters");
+    // return ""
+    // }    
+    let pwdOptionUpperCase = window.prompt("Click Ok to include uppercase.");
+    let pwdOptionLowerCase = window.prompt("Click Ok to include lowercase.");
+    let pwdOptionpwdNumber = window.prompt("Click Ok to include number.");
+    let pwdOptionSpecialChar = window.prompt("Click Ok to include special charcter.");
 
+    // Array for all character option for password
+    let pwdUpperCase = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    let pwdLowerCase = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let pwdNumber = ["0", "1","2","3","4","5","6","7","8","9"];
+    let pwdSpecialChar = ["!", "@","#","$","%","^","&","*","(",")"];
+
+    let pwdOptions = [];
+
+    if (pwdOptionUpperCase === true) {
+        pwdOptions.push(pwdOptionUpperCase)
+    }
+
+    if (pwdOptionLowerCase === true) {
+        pwdOptions.push(pwdOptionLowerCase)
+    }
+
+    if (pwdOptionpwdNumber === true) {
+        pwdOptions.push(pwdOptionpwdNumber)
+    }
+
+    if (pwdOptionSpecialChar === true) {
+        pwdOptions.push(pwdOptionSpecialChar)
+    }
+    let generatePassword = ""
+
+    for (let i = 0; i < passwordLength; i++){
+        let randomInt = getRandomItem(pwdOptions)
+        let randomChar = getRandomItem()
+        generatePassword += randomChar
+    }
+
+   return generatePassword 
+
+}
+        
 
 // Write password to the #password input
 function writePassword() {
@@ -24,36 +67,8 @@ function writePassword() {
     passwordText.value = password;
 }    
 
-function generatePassword() { 
-    let passwordLength = prompt("Password must be between 8 and 128 characters. How long do you want your password to be?");
-    console.log(passwordLength);
-
-
-    if (passwordLength<8) {
-    alert("Password minimum is 8 characters");
-    return ""
-} 
-    else if (passwordLength>128) {
-    alert("Password maximum is 128 characters");
-    return ""
-    }    
-    let passwordUpperCase = prompt("Click Ok to include uppercase.");
-    let passwordLowerCase = prompt("Click Ok to include lowercase.");
-    let passwordNumbers = prompt("Click Ok to include number.");
-    let passwordspecialChar = prompt("Click Ok to include special charcter.");
-}
-
-
-
-
-
-
-
-
-
-
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
